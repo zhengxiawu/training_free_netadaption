ecfac6fa4f5cc501e56e9307a87c56f48921becd
zhengxiawu <zhengxiawu@126.com>
2020-12-30 19:43:23 +0800

    bug fixing

--- CAP/run_scripts/train/train_vgg_150m.sh
+++ CAP/run_scripts/train/train_vgg_150m.sh
@@ -4,7 +4,7 @@ python3 -m torch.distributed.launch --nproc_per_node=1 train.py --train \
  --cfg "[64, 46, 82, 94, 174, 182, 172, 354, 204, 376, 266, 512, 460]" \
  --path "Exp_train/train_vgg_150m" \
  --dataset "cifar10" \
- --save_path "/userhome/data/cifar10" \
+ --save_path "/gdata/cifar10" \
  --base_path "Exp_base/vgg_base" \
  --warm_epoch 1 \
  --n_epochs 300 \
--- CAP/run_scripts/train/train_vgg_70m.sh
+++ CAP/run_scripts/train/train_vgg_70m.sh
@@ -4,7 +4,7 @@ python3 -m torch.distributed.launch --nproc_per_node=1 train.py --train \
  --cfg "[22, 34, 58, 72, 152, 126, 118, 270, 198, 202, 260, 102, 216]" \
  --path "Exp_train/train_vgg_70m" \
  --dataset "cifar10" \
- --save_path "/userhome/data/cifar10" \
+ --save_path "/gdata/cifar10" \
  --base_path "Exp_base/vgg_base" \
  --warm_epoch 1 \
  --n_epochs 300 \
